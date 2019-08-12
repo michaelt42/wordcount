14987fa3e0f109e797f73acbcf9574ea9d3b7f26
Michael Tourigny <michaelt@terawe.com>
2019-08-12 14:23:11 -0700

    Added docker files

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM ubuntu
+
+ADD client.sh /usr/wc/client.sh
+ADD https://www.apache.org/dist/kafka/2.3.0/kafka_2.12-2.3.0.tgz /usr/kafka.tgz
+ADD tweets.csv /usr/wc/tweets.csv
+
+ENV KAFKA="broker" \
+    MODE="" \
+    PORT="29092"
+
+#CMD ["/bin/bash /usr/wc/client.sh"]
+
+RUN apt-get -y update && \
+    apt-get -y install csvtool default-jre && \
+    export T="$(gunzip -l /usr/kafka.tgz | awk '/^ *[0-9]/{print $4}')" && \
+    gunzip /usr/kafka.tgz && \
+    tar -C /usr -f $T -x > /dev/null && \
+    mv -f "/usr/$(tar -tf $T | head -n 1 | sed 's#/.*##')" /usr/kafka && \
+    rm $T
---
+++ client.sh
@@ -0,0 +1,35 @@
+PATH="${PATH}:/usr/kafka/bin"
+if [[ 'consumer' == $MODE ]]
+then
+    echo "Execute consumer against ${KAFKA}:${PORT}"
+    #kafka-console-consumer.sh --bootstrap-server $KAFKA:$PORT --topic wc-counts
+    exit 0
+fi
+if [[ 'counter' == $MODE ]]
+then
+    echo "Execute counter against ${KAFKA}:${PORT}"
+    #while true
+    #do
+    #    raw="$(kafka-console-consumer.sh --bootstrap-server $KAFKA:$PORT --isolation-level read_committed --max-messages 1 --topic wc-tweets)"
+    #    a="$(echo $raw | csvtool -t ',' col 2 - | sed 's/^"//;s/"$//')"
+    #    c="$(echo $raw | csvtool -t ',' col 6 - | sed 's/\(.*\)/\L\1/;s/[\t !"(),.?]/\n/g' | sed '/^$/d' | sort | uniq -c | sed 's/\n/\\n/g' | sed 's/^"//;s/"$//')"
+    #    l="$(echo $raw | csvtool -t ',' col 8 - | sed 's/^"//;s/"$//')"
+    #    if [[ -z "${l}" ]]
+    #    then
+    #        l="Unknown location"
+    #    fi
+    #    n="$(echo $raw | csvtool -t ',' col 4 - | sed 's/^"//;s/"$//')"
+    #    echo -e "${n} from ${l} on ${a}\n${c}\n" | kafka-console-producer.sh --broker-list $KAFKA:$PORT --topic wc-counts
+    #done
+    exit 0
+fi
+if [[ 'producer' == $MODE ]]
+then
+    echo "Execute producer against ${KAFKA}:${PORT}"
+    #while true
+    #do
+    #    shuf -n1 /usr/wc/Tweets.csv | kafka-console-producer.sh --broker-list $KAFKA:$PORT --topic wc-tweets
+    #    sleep 15
+    #done
+    exit 0
+fi
